251273edbfd2b9f7cacdd349f86c8d2050a61378
Jefferson-Bantawig <>
2024-01-07 10:37:16 +0000

    Add choices to next question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,86 +3,87 @@ const howToPlay = document.querySelector(".how-to-play-div");
 const home = document.querySelector(".home-container");
 const gamePage = document.querySelector(".main-container");
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+const choices = Array.from(document.getElementsByClassName("choice-text")); /** This converts the choices text from an Html collection to an array */
 const correctPoints = 1;
 const maxQuestions = 10;
 let currentQuestion = {};
 let correctAnswer = true;
 let score = 0;
-let questionCounter = 0;
+let questionCounter = document.getElementById("question-page");
+let questionNumber = document.getElementById("question-num")
 let availableQuestions = [];
 
 let questions = [
     {
         question: "What is the fastest animal on Earth?",
-        choiceA: "Cheetah",
-        choiceB: "Ostrich",
-        choiceC: "Gazelle",
-        choiceD: "Peregrine Falcon",
+        choice1: "Cheetah",
+        choice2: "Ostrich",
+        choice3: "Gazelle",
+        choice4: "Peregrine Falcon",
         answer: 4
     },
     {
         question: "What is the capital of Australia?",
-        choiceA: "Perth",
-        choiceB: "Melbourne",
-        choiceC: "Canberra",
-        choiceD: "Sydney",
+        choice1: "Perth",
+        choice2: "Melbourne",
+        choice3: "Canberra",
+        choice4: "Sydney",
         answer: 3
     },
     {
         question: "What year did World War 2 start?",
-        choiceA: "1921",
-        choiceB: "1939",
-        choiceC: "1914",
-        choiceD: "1945",
+        choice1: "1921",
+        choice2: "1939",
+        choice3: "1914",
+        choice4: "1945",
         answer: 2
     },
     {
         question: "How many ribs do most humans have?",
-        choiceA: "12",
-        choiceB: "24",
-        choiceC: "14",
-        choiceD: "22",
+        choice1: "12",
+        choice2: "24",
+        choice3: "14",
+        choice4: "22",
         answer: 2
     },
     {
         question: "What is the smallest planet in the solar system?",
-        choiceA: "Pluto",
-        choiceB: "Venus",
-        choiceC: "Mars",
-        choiceD: "Mercury",
+        choice1: "Pluto",
+        choice2: "Venus",
+        choice3: "Mars",
+        choice4: "Mercury",
         answer: 4
     },
     {
         question: "In which city is the Cathedral of Notre Dame?",
-        choiceA: "Paris",
-        choiceB: "Marseille",
-        choiceC: "Nice",
-        choiceD: "Montpellier",
+        choice1: "Paris",
+        choice2: "Marseille",
+        choice3: "Nice",
+        choice4: "Montpellier",
         answer: 1
     },
     {
         question: "What does the roman numeral 'C' represent?",
-        choiceA: "100",
-        choiceB: "1000",
-        choiceC: "2000",
-        choiceD: "20000",
+        choice1: "100",
+        choice2: "1000",
+        choice3: "2000",
+        choice4: "20000",
         answer: 1
     },
     {
         question: "What is the hardest natural material on earth?",
-        choiceA: "Adamantium",
-        choiceB: "Vibranium",
-        choiceC: "Diamond",
-        choiceD: "Delirium",
+        choice1: "Adamantium",
+        choice2: "Vibranium",
+        choice3: "Diamond",
+        choice4: "Delirium",
         answer: 3
     },
     {
         question: "Who invented the first car?",
-        choiceA: "Henry Ford",
-        choiceB: "Walter Owen Bentley",
-        choiceC: "Ferdinand Porsche",
-        choiceD: "Karl Benz",
+        choice1: "Henry Ford",
+        choice2: "Walter Owen Bentley",
+        choice3: "Ferdinand Porsche",
+        choice4: "Karl Benz",
         answer: 4
     },
 ]
@@ -99,6 +100,23 @@ function startGame() {
         gamePage.classList.remove("hide")
         home.setAttribute("class", "hide")
     }, 1000);
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions] /**This makes a new array with the same content as the questions array, but by doing it this way, you can modify one array without changing the other array */
+    getNewQuestion()
+}
+
+function getNewQuestion() {
+    questionCounter++;
+    questionNumber++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question; /** This changes the inner text in the html question tag. "currentQuestion.question" -> this gets the property "question" that is inside the questions array */
+    
+    choices.forEach( choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number]
+    })
 }
 
 /** This function sets the home button to reload to landing page */
