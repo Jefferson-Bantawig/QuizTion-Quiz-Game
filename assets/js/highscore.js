f650f396bfde1bcf3edc668231f5d0570e85bdff
Jefferson-Bantawig <>
2024-01-20 16:32:09 +0000

    Add aria labels to all html files

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,7 +1,7 @@
 const topScores = document.getElementById("top-scores");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-topScores.innerHTML = highScores.map((score, index) => {
+topScores.innerHTML = highScores.map((score, index) => { //map() takes 2 parameters, (current element, index of the current element in the array)
     return `<li class="top-score-list">
        ${index + 1 + "."} ${score.name} - ${score.score}
        </li>`;
