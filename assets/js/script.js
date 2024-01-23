440ef783ea7f782bb1c133fba08cf32fa37ca83e
Jefferson-Bantawig <>
2024-01-23 21:27:07 +0000

    fix console log bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -111,7 +111,6 @@ function startGame() {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions);
     getNewQuestion();
     progressBar.style.width = "0%";
 }
